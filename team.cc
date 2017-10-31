b0e9375f9c35c669651e794c1c6b8ea103da45af
christiealtadonna <32642875+christiealtadonna@users.noreply.github.com>
2017-10-31 14:54:29 -0400

    Initial commit

---
+++ team.cc
@@ -0,0 +1,12 @@
+#include "support.h"
+
+/*
+ * Please be sure to fill in this struct right away!
+ */
+struct team_t team =
+{
+	"Christie Altadonna", /* first member name, e.g., Michael Spear */
+	"cha415@lehigh.edu", /* first member email, e.g., mfs409@lehigh.edu */
+	"Timothy Berrill", /* second member name, e.g., Michael Spear */
+	"tjb318@lehigh.edu"  /* second member email, e.g., mfs409@lehigh.edu */
+};
